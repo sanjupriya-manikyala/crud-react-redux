cbc84889ed049844a0631722d1c7925edac05b69
Sanjupriya Manikyala <sanjupriya.manikyala@cesltd.com>
2021-08-30 19:24:27 +0530

    Configure Redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+
+const store = createStore(applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
